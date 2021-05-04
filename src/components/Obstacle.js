1bb4e256b485c6c8661ad7c8309e750fdf5d77aa
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-05-04 17:29:33 -0400

    varying height

--- src/components/Obstacle.js
+++ src/components/Obstacle.js
@@ -2,23 +2,26 @@ import React from 'react';
 import {View} from 'react-native';
 import {array, object, string} from 'prop-types';
 import Matter from 'matter-js';
+const RandomNumber = Math.floor(Math.random() * 65) + 5 ;
 
+console.log(RandomNumber)
 const Obstacle = props => {
   const width = props.size[0];
   const height = props.size[1];
   const x = props.body.position.x - width / 2;
   const y = props.body.position.y - height / 2;
+
   return (
     <View
       style={[
         {
           position: 'absolute',
           left: x,
-          top: y,
-          width: width,
+          top: RandomNumber + "%",
+          width: width/2,
           borderRadius: 20,
           height: height,
-          backgroundColor: "red"
+          backgroundColor: "gold"
         },
       ]}
     />
